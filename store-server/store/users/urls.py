488ebedfa5407c7f0a3b187095e90ac438d68ba5
Kopylov Andrey <andrey.kopylov.03@bk.ru>
2023-06-12 21:01:45 +0300

    a personal account window has been created, error handling during registration and authorization has been added

--- store-server/store/users/urls.py
+++ store-server/store/users/urls.py
@@ -2,12 +2,14 @@
 from django.urls import path
 
 
-from users.views import login, registration
+from users.views import login, registration, profile, logout
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),
-    path('registration/', registration, name='registration')
+    path('registration/', registration, name='registration'),
+    path('profile/', profile, name='profile'),
+    path('logout/', logout, name='logout')
 ]
 
--- store-server/store/users/views.py
+++ store-server/store/users/views.py
@@ -1,11 +1,64 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth, messages
+from django.urls import reverse
+
+
 
 from users.models import User
-from users.forms import UserLoginForm
-
+from users.forms import UserLoginForm, UserRegistrationForms, UserProfileForm
+@csrf_exempt
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username=request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
 
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
+@csrf_exempt
 def registration(request):
-    return render(request, 'users/registration.html')
+    if request.method == 'POST':
+        form = UserRegistrationForms(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегистрировались!')
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForms()
+    context = {'form': form}
+
+    return render(request, 'users/registration.html', context)
+
+@csrf_exempt
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user,  data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            print(form.errors)
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {'title': 'Store - Профиль', 'form': form}
+    return render(request, 'users/profile.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+
+
+
+
+
