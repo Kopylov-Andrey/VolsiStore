4291af3c1510963ddf95755bb35fc59b8a89417b
Kopylov Andrey <andrey.kopylov.03@bk.ru>
2023-06-12 13:33:23 +0300

    added registration and authorization form

---
+++ store-server/store/users/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+
+
+from users.views import login, registration
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', login, name='login'),
+    path('registration/', registration, name='registration')
+]
+
--- store-server/store/users/views.py
+++ store-server/store/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from users.models import User
+from users.forms import UserLoginForm
+
+def login(request):
+    return render(request, 'users/login.html')
+
+
+def registration(request):
+    return render(request, 'users/registration.html')
